630354a42f34cecfe3b84058945e68947c284d7c
Exstaa <exstagithub@gmail.com>
2024-10-28 17:45:07 +0400

    main class

---
+++ az/hafizrzazade/urlshortener/UrlShortenerMainClass.java
@@ -0,0 +1,11 @@
+package az.hafizrzazade.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerMainClass {
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerMainClass.class, args);
+	}
+}
